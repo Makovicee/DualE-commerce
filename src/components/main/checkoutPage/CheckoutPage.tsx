66dc0b9b3afbf0bd72bca0d3c1270eb43e1cfd81
husnik <martinex.husnik@seznam.cz>
2026-03-20 18:07:28 +0100

    feat:UI-AST Checkout controls

--- src/components/main/checkoutPage/CheckoutPage.tsx
+++ src/components/main/checkoutPage/CheckoutPage.tsx
@@ -1,37 +1,52 @@
-import { Box, SimpleGrid, Stepper, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Group,
+  SimpleGrid,
+  Stack,
+  Stepper,
+  Text,
+} from "@mantine/core";
 import { useUIMode } from "../../../contexts/UIModeContext";
 import Payment from "./payment/Payment";
 import Delivery from "./delivery/Delivery";
 import Cart from "./cart/Cart";
-import { Car, CreditCard, ShoppingCart } from "lucide-react";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Car,
+  Check,
+  CreditCard,
+  ShoppingCart,
+} from "lucide-react";
 import { useState } from "react";
 
-const EFF_ORDER = [
-  { label: "1. Doprava", component: <Delivery /> },
-  { label: "2. Platební údaje", component: <Payment /> },
-  { label: "3. Položky", component: <Cart /> },
-];
-
-const AST_STEPS = [
+const STEPS = [
   {
     label: "Košík",
     description: "Přehled položek",
     icon: ShoppingCart,
+    effLabel: "3. Položky",
     component: <Cart />,
   },
   {
     label: "Doprava",
     description: "Detaily doručení",
     icon: Car,
+    effLabel: "1. Doprava",
     component: <Delivery />,
   },
   {
     label: "Platba",
     description: "Zaplacení objednávky",
     icon: CreditCard,
+    effLabel: "2. Platební údaje",
     component: <Payment />,
   },
 ];
+
+const EFF_ORDER = [STEPS[1], STEPS[2], STEPS[0]];
 
 const CheckoutPage = () => {
   const { mode } = useUIMode();
@@ -55,33 +70,65 @@ const CheckoutPage = () => {
       ))}
     </SimpleGrid>
   ) : (
-    <Stepper
-      m="xl"
-      color="green"
-      active={active}
-      w="75%"
-      mx="auto"
-      onStepClick={handleStepClick}
-    >
-      {AST_STEPS.map((step) => (
-        <Stepper.Step
-          key={step.label}
-          icon={<step.icon size={18} style={{ display: "block" }} />}
-          label={step.label}
-          description={step.description}
-        >
-          <Box
-            mt="xl"
-            key={key}
-            style={{
-              animation: "stepIn 1000ms ease-out",
-            }}
+    <Stack>
+      <Stepper
+        m="xl"
+        color="green"
+        active={active}
+        w="75%"
+        mx="auto"
+        onStepClick={handleStepClick}
+      >
+        {STEPS.map((step) => (
+          <Stepper.Step
+            key={step.label}
+            icon={<step.icon size={18} style={{ display: "block" }} />}
+            label={step.label}
+            description={step.description}
           >
-            {step.component}
-          </Box>
-        </Stepper.Step>
-      ))}
-    </Stepper>
+            <Box
+              mt="xl"
+              key={key}
+              style={{
+                animation: "stepIn 1000ms ease-out",
+              }}
+            >
+              {step.component}
+            </Box>
+          </Stepper.Step>
+        ))}
+        <Stepper.Completed>
+          <Text ta="center" mt="xl">
+            Objednávka dokončena!
+          </Text>
+        </Stepper.Completed>
+      </Stepper>
+
+      <Group justify="center" mb="xl">
+        <ActionIcon
+          variant="light"
+          disabled={active === 0}
+          onClick={() => handleStepClick(active - 1)}
+          size={"input-md"}
+        >
+          <ArrowLeft size={22} />
+        </ActionIcon>
+        <Button
+          color="grape"
+          onClick={() => handleStepClick(active + 1)}
+          disabled={active === STEPS.length}
+          rightSection={
+            active === STEPS.length - 1 ? (
+              <Check size={22} />
+            ) : (
+              <ArrowRight size={22} />
+            )
+          }
+        >
+          {active === STEPS.length - 1 ? "Dokončit" : "Pokračovat"}
+        </Button>
+      </Group>
+    </Stack>
   );
 };
 export default CheckoutPage;
