5c2ddc5009f7eb64cdf4194f700e127c4a63debd
husnik <martinex.husnik@seznam.cz>
2026-04-02 18:02:10 +0200

    fix:EFF adjusments

--- src/components/main/checkoutPage/payment/paymentInfo/PaymentInfo.tsx
+++ src/components/main/checkoutPage/payment/paymentInfo/PaymentInfo.tsx
@@ -1,8 +1,18 @@
-import { Group, Stack, TextInput } from "@mantine/core";
+import { Group, Stack, TextInput, Kbd, Text, Divider } from "@mantine/core";
 import type { CheckoutFormProps } from "../../../../../core/logic/useCheckoutForm";
 import { useRef } from "react";
+import { useHotkeys } from "@mantine/hooks";
+import { useCart } from "../../../../../core/CartContext";
 
 const PaymentInfo = ({ form }: CheckoutFormProps) => {
+  const { clearCart, undo, redo } = useCart();
+
+  useHotkeys([
+    ["mod+z", undo],
+    ["mod+y", redo],
+    ["mod+d", clearCart],
+  ]);
+
   const formatCardNumber = (value: string) =>
     value
       .replace(/\D/g, "")
@@ -23,47 +33,72 @@ const PaymentInfo = ({ form }: CheckoutFormProps) => {
   const nameRef = useRef<HTMLInputElement>(null);
 
   return (
-    <Stack>
-      <TextInput
-        ref={cardRef}
-        withAsterisk
-        label="Karta"
-        placeholder="1234 5678 9012 3456"
-        {...form.getInputProps("cardNumber")}
-        onChange={(e) => {
-          const formatted = formatCardNumber(e.target.value);
-          form.setFieldValue("cardNumber", formatted);
-          if (formatted.replace(/\s/g, "").length === 16)
-            expiryRef.current?.focus();
-        }}
-      />
-      <Group grow>
+    <Stack justify="space-between" h={"100%"}>
+      <Stack>
         <TextInput
-          ref={expiryRef}
-          placeholder="MM/YY"
-          {...form.getInputProps("cardExpiry")}
+          ref={cardRef}
+          withAsterisk
+          label="Karta"
+          placeholder="1234 5678 9012 3456"
+          {...form.getInputProps("cardNumber")}
           onChange={(e) => {
-            const formatted = formatExpiry(e.target.value);
-            form.setFieldValue("cardExpiry", formatted);
-            if (formatted.length === 5) cvcRef.current?.focus();
+            const formatted = formatCardNumber(e.target.value);
+            form.setFieldValue("cardNumber", formatted);
+            if (formatted.replace(/\s/g, "").length === 16)
+              expiryRef.current?.focus();
           }}
         />
+        <Group grow>
+          <TextInput
+            ref={expiryRef}
+            placeholder="MM/YY"
+            {...form.getInputProps("cardExpiry")}
+            onChange={(e) => {
+              const formatted = formatExpiry(e.target.value);
+              form.setFieldValue("cardExpiry", formatted);
+              if (formatted.length === 5) cvcRef.current?.focus();
+            }}
+          />
+          <TextInput
+            ref={cvcRef}
+            placeholder="CVC"
+            {...form.getInputProps("cardCVC")}
+            onChange={(e) => {
+              const value = e.target.value.replace(/\D/g, "").slice(0, 3);
+              form.setFieldValue("cardCVC", value);
+              if (value.length === 3) nameRef.current?.focus();
+            }}
+          />
+        </Group>
         <TextInput
-          ref={cvcRef}
-          placeholder="CVC"
-          {...form.getInputProps("cardCVC")}
-          onChange={(e) => {
-            const value = e.target.value.replace(/\D/g, "").slice(0, 3);
-            form.setFieldValue("cardCVC", value);
-            if (value.length === 3) nameRef.current?.focus();
-          }}
+          ref={nameRef}
+          placeholder="Jméno příjmení"
+          {...form.getInputProps("cardName")}
         />
-      </Group>
-      <TextInput
-        ref={nameRef}
-        placeholder="Jméno příjmení"
-        {...form.getInputProps("cardName")}
-      />
+      </Stack>
+      <Stack align="center">
+        <Divider w={"50%"} label="Zkratky košíku" />
+        <Group gap={"xl"} justify="center">
+          <Stack gap={"xs"} align="center">
+            <Text size="sm" c="dimmed">
+              Smazat
+            </Text>
+            <Kbd>⌘ + D</Kbd>
+          </Stack>
+          <Stack gap={"xs"} align="center">
+            <Text size="sm" c="dimmed">
+              Navrátit
+            </Text>
+            <Kbd>⌘ + Y</Kbd>
+          </Stack>
+          <Stack gap={"xs"} align="center">
+            <Text size="sm" c="dimmed">
+              Vrátit
+            </Text>
+            <Kbd>⌘ + Z</Kbd>
+          </Stack>
+        </Group>
+      </Stack>
     </Stack>
   );
 };
